2a243b965a69e87ded237e861bad40267259fc94
Kundan0-prajapati <170390107034@saffrony.ac.in>
2020-03-08 17:14:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+for(var i=0;i<7;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+      var buttonInput = this.innerHTML;
+      makeSound(buttonInput);    
+      buttonAnimation(buttonInput);
+    });
+}
+
+document.addEventListener("keypress",function(event){
+     makeSound(event.key);
+     buttonAnimation(event.key);
+})
+
+ function makeSound(key){
+
+    switch (key) {
+        
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+      
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;              
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var tom5 = new Audio("sounds/crash.mp3");
+            tom5.play();
+            break;
+
+        case "k":
+            var tom6 = new Audio("sounds/snare.mp3");
+            tom6.play();
+            break;
+
+        case "l":
+            var tom7 = new Audio("sounds/kick-bass.mp3");
+            tom7.play();
+            break;
+            
+          default:
+              console.log(key);
+              break;
+      }
+ }
+
+ function buttonAnimation(key){
+     var activebutton = document.querySelector("." + key);
+     activebutton.classList.add("pressed");
+
+     setTimeout(() => {
+         activebutton.classList.remove("pressed");
+     }, 200);
+ }
